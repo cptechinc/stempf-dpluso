5cdc62388b3789032a6144d986d494dbeae7b571
bbullemer <barbara.bullemer@gmail.com>
2018-06-14 13:39:48 -0500

    Vendor Contact Fix
    - fixed vendor contact error
    
    (cherry picked from commit cptechinc/soft-dpluso@42bf58c34a01d0103c272ea408197b51aac11112)

--- site/templates/content/vend-information/vend-contact.php
+++ site/templates/content/vend-information/vend-contact.php
@@ -9,24 +9,28 @@
 	if (file_exists($contactfile)) {
 		// JSON file will be false if an error occurred during file_get_contents or json_decode
 		$contactjson = json_decode(file_get_contents($contactfile), true);
-		$contactjson = $contactjson ? $contactjson : array('error' => true, 'errormsg' => 'The Vendor Contacts JSON contains errors. JSON ERROR: '.json_last_error()); 
+		$contactjson = $contactjson ? $contactjson : array('error' => true, 'errormsg' => 'The Vendor Contacts JSON contains errors. JSON ERROR: '.json_last_error());
 
 		if ($contactjson['error']) {
-			echo $page->bootstrap->createalert('warning', $contactjson['errormsg']); 
+			echo $page->bootstrap->createalert('warning', $contactjson['errormsg']);
 		} else {
 			$vendorleftcolumns = array_keys($contactjson['columns']['vendor']['vendorleft']);
 			$vendorrightcolumns = array_keys($contactjson['columns']['vendor']['vendorright']);
-			$contactcolumns = array_keys($contactjson['columns']['contact']);
-			
+			if (isset($contactjson['columns']['contact'])) {
+				$contactcolumns = array_keys($contactjson['columns']['contact']);
+			} else {
+				$contactcolumns = false;
+			}
+
 			if (!empty($contactjson['data']['shipfm'])) {
 				$shipfmleftcolumns = array_keys($contactjson['columns']['shipfm']['shipfmleft']);
 				$shipfmrightcolumns = array_keys($contactjson['columns']['shipfm']['shipfmright']);
 			}
-			
+
 			if (isset($contactjson['columns']['forms']))  {
 				$formscolumns = array_keys($contactjson['columns']['forms']);
 			}
-			
+
 			if (sizeof($contactjson['data']) > 0) {
 				echo '<div class="row">';
 					echo '<div class="col-sm-6">';
@@ -82,36 +86,40 @@
 				}
 
 				echo '<h2>Vendor Contact Info</h2>';
-				$tb = new Table('class=table table-striped table-bordered table-condensed table-excel');
-					$tb->tablesection('thead');
-					$tb->tr();
-						foreach ($contactcolumns as $column) {
-							$tb->th('class='.$config->textjustify[$contactjson['columns']['contact'][$column]['headingjustify']], $contactjson['columns']['contact'][$column]['heading']);
-						}
-					$tb->closetablesection('thead');
-					$tb->tablesection('tbody');
-						foreach ($contactjson['data']['contact'] as $contact) {
-							$tb->tr();
-							$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactname']['datajustify']], $contact['contactname']);
-							$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactemail']['datajustify']], $contact['contactemail']);
-							if (isset($contact['contactnumbers']["1"]['contactnbr'])) {
-								$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactnbr']['datajustify']], $contact['contactnumbers']["1"]['contactnbr']);
-							} else {
-								$tb->td();
+				if (isset($contactjson['data']['contact'])) {
+					$tb = new Table('class=table table-striped table-bordered table-condensed table-excel');
+						$tb->tablesection('thead');
+						$tb->tr();
+							foreach ($contactcolumns as $column) {
+								$tb->th('class='.$config->textjustify[$contactjson['columns']['contact'][$column]['headingjustify']], $contactjson['columns']['contact'][$column]['heading']);
 							}
-							for ($i = 1; $i < sizeof($contact['contactnumbers']) + 1; $i++) {
-								if ($i != 1) {
-									$tb->tr();
+						$tb->closetablesection('thead');
+						$tb->tablesection('tbody');
+							foreach ($contactjson['data']['contact'] as $contact) {
+								$tb->tr();
+								$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactname']['datajustify']], $contact['contactname']);
+								$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactemail']['datajustify']], $contact['contactemail']);
+								if (isset($contact['contactnumbers']["1"]['contactnbr'])) {
+									$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactnbr']['datajustify']], $contact['contactnumbers']["1"]['contactnbr']);
+								} else {
 									$tb->td();
-									$tb->td();
-									$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactnbr']['datajustify']], $contact['contactnumbers']["$i"]['contactnbr']);
+								}
+								for ($i = 1; $i < sizeof($contact['contactnumbers']) + 1; $i++) {
+									if ($i != 1) {
+										$tb->tr();
+										$tb->td();
+										$tb->td();
+										$tb->td('class='.$config->textjustify[$contactjson['columns']['contact']['contactnbr']['datajustify']], $contact['contactnumbers']["$i"]['contactnbr']);
+									}
 								}
 							}
-						}
-					$tb->closetablesection('tbody');
-				echo $tb->close();
-				
-				if (isset($contactjson['columns']['forms'])) { 
+						$tb->closetablesection('tbody');
+					echo $tb->close();
+				} else {
+					echo '<p>No Contacts Found</p>';
+				}
+
+				if (isset($contactjson['columns']['forms'])) {
 					echo '<h2>Forms Information</h2>';
 					$tb = new Table('class=table table-striped table-bordered table-condensed table-excel');
 						$tb->tablesection('thead');
@@ -131,10 +139,10 @@
 					echo $tb->close();
 				}
 			} else {
-				echo $page->bootstrap->createalert('warning', 'Information Not Available'); 
+				echo $page->bootstrap->createalert('warning', 'Information Not Available');
 			} // END if (sizeof($contactjson['data']) > 0)
 		}
 	} else {
-		echo $page->bootstrap->createalert('warning', 'Vendor has no Contacts'); 
+		echo $page->bootstrap->createalert('warning', 'Vendor has no Contacts');
 	}
 ?>
