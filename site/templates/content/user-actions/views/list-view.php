3efa0bdc393af503a4523aec98c2ec8fcd9d3cab
Paul Gomez <pauldro952@outlook.com>
2018-06-18 14:22:30 -0500

    Fix clear filter button on list view
    (cherry picked from commit cptechinc/soft-dpluso@13bd1e3f98926dd120aab64dddd67e06f1ac14a8)

--- site/templates/content/user-actions/views/list-view.php
+++ site/templates/content/user-actions/views/list-view.php
@@ -53,7 +53,7 @@
 				</div>
 				<div class="col-xs-6">
 					<?php if ($input->get->filter) : ?>
-						<a href="#" class="btn btn-sm btn-warning btn-block"><i class="fa fa-times" aria-hidden="true"></i> Clear Filter</a>
+						<a href="<?= $actionpanel->generate_clearfilterurl(); ?>" class="btn btn-sm btn-warning btn-block load-link" data-loadinto="<?= $actionpanel->loadinto; ?>" data-focus="<?= $actionpanel->focus; ?>"><i class="fa fa-times" aria-hidden="true"></i> Clear Filter</a>
 					<?php endif; ?>
 				</div>
 			</div>
