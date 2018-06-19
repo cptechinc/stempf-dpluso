8b23049b1c18eb5d6171125f3dd11456a05d90ff
bbullemer <barbara.bullemer@gmail.com>
2018-06-19 11:23:17 -0500

    Customer table sort
    - fixed issue of pulling in user quotes when table was sorted
    
    (cherry picked from commit cptechinc/soft-dpluso@58925094d5f35a7e8a7a77c8c7789f955e4f184b)

--- site/templates/content/ajax/load/quotes-router.php
+++ site/templates/content/ajax/load/quotes-router.php
@@ -1,6 +1,7 @@
 <?php
 	$filteron = $input->urlSegment(2);
-	if ($input->get->qnbr) { $qnbr = $input->get->text('qnbr'); } else { $qnbr = NULL; }
+	$qnbr = $input->get->text('qnbr');
+	
 	switch ($filteron) {
 		case 'cust':
 			$custID = $sanitizer->text($input->urlSegment(3));
@@ -12,8 +13,17 @@
 			}
 			$page->body = $config->paths->content.'customer/cust-page/quotes/quotes-panel.php';
 			break;
-		case 'search': //TODO
-			//$include = $config->paths->content.'recent-orders/ajax/load/order-search-modal.php'; //FIX
+		case 'customer':
+			$custID = $sanitizer->text($input->urlSegment(3));
+			$shipID = '';
+			if ($input->urlSegment(4)) {
+				if (strpos($input->urlSegment(4), 'shipto') !== false) {
+					$shipID = str_replace('shipto-', '', $input->urlSegment(4));
+				}
+			}
+			$page->body = $config->paths->content.'customer/cust-page/quotes/quotes-panel.php';
+			break;
+		case 'search':
             $searchtype = $sanitizer->text($input->urlSegment(3));
             switch ($searchtype) {
                 case 'cust':
